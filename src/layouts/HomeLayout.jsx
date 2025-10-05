c76fa53c3653081bd931780400099d3d33750b9e
Your Name <you@example.com>
2025-10-05 19:14:55 +0600

    first commit

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/navigations/NavBar';
+
+const HomeLayout = () => {
+    return (
+        <div>
+        <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default HomeLayout;
